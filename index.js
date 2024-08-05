25ecb6979feed4aefdec8f3e7dde7f1f6acfe6ec
WildH0g <dmitry.kostyuk@gmail.com>
2024-08-05 13:17:04 +0200

    Implement CLI code

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+const REPO = 'git@github.com:WildH0g/apps-script-engine-template.git';
+
+const COMMANDS = {
+  gitClone(dir = '') {
+    return `git clone ${REPO} ${dir}`;
+  },
+};
+
+const dir = process.argv[2] || 'apps-script-engine';
+try {
+  console.log(`Initiating Apps Script Engine in directory "${dir}"`);
+  execSync(COMMANDS.gitClone(dir));
+
+  const folderPath = path.join(process.cwd(), dir);
+
+  fs.rm(
+    path.join(folderPath, '.git'),
+    { recursive: true, force: true },
+    () => {}
+  );
+
+  fs.rm(path.join(folderPath, 'README.md'), () => {});
+  fs.rm(path.join(folderPath, 'HISTORY.md'), () => {});
+
+  console.log(`✅ Success!`);
+} catch (err) {
+  console.error('❌ Something went wrong: ${err}');
+}
